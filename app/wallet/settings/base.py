b168d5dcd4344dfed1f77b3a57293463923eb3d4
akhilrs <akhil.rs@inapp.com>
2016-05-18 18:33:48 +0530

    Configuration changes

--- app/wallet/settings/base.py
+++ app/wallet/settings/base.py
@@ -103,4 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, 'assets', 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIR, 'assets', 'media')
