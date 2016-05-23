62d43126cabb26b88811421c53951e58774b5459
akhilrs <akhilrs@live.com>
2016-05-23 07:02:50 +0530

    Model and admin changes

--- app/wallet/chat/admin.py
+++ app/wallet/chat/admin.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
+from django_mongoengine import mongo_admin as admin
 from wallet.chat.models import ChatInvite
 
-admin.site.register(ChatInvite)
+
+@admin.register(ChatInvite)
+class ChatInviteAdmin(admin.DocumentAdmin):
+    pass
