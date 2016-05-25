7946f83613e544376a85dd74668f9d335bdeda15
akhilrs <akhilrs@live.com>
2016-05-25 06:56:05 +0530

    Urls and Viewset changes

--- app/wallet/chat/views.py
+++ app/wallet/chat/views.py
@@ -4,12 +4,20 @@ from django.db import transaction
 from django.shortcuts import render, redirect
 import haikunator
 from .models import Room
+from .models import ChatInvite
 from rest_framework.views import APIView
+from rest_framework import status
+from wallet.core.http import HttpHandler as http
 
 
 class ChatInviteView(APIView):
     def post(self, request):
         try:
-            pass
+            invite = ChatInvite.objects.create(
+                sender_id=request.data['sender'],
+                recipient_id=request.data['recipient'],
+            )
+            invite.save()
+            return http.json_response_wrapper([], 'Success', status.HTTP_200_OK)
         except Exception as e:
-            return e.message
+            return http.json_response_wrapper([], e.message, status.HTTP_200_OK)
